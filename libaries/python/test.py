85f812b1d93983fa44ce60a266d7033b930b2214
Adam Haile <adammhaile@gmail.com>
2017-02-22 19:11:17 -0500

    tweaked test

--- libaries/python/test.py
+++ libaries/python/test.py
@@ -2,7 +2,7 @@ from super7 import Super7, BaudRates
 import serial.tools.list_ports
 import time
 
-TOTAL_CHARS = 24
+TOTAL_CHARS = 36
 
 s7 = Super7(serial.tools.list_ports.comports()[0].device, baudrate=BaudRates.BAUD_9600)
 
@@ -10,7 +10,7 @@ msg = ([' ']*TOTAL_CHARS) + [chr(i) for i in range(32, 123)]
 msg = ''.join(msg)
 for i in range(len(msg) + 1):
     s7.write(msg[i:i+TOTAL_CHARS])
-    time.sleep(0.2)
+    time.sleep(0.1)
 
 time.sleep(1)
 
@@ -18,6 +18,11 @@ for i in range(0, 256 - TOTAL_CHARS):
     s7.send_raw([i+a for a in range(TOTAL_CHARS)])
     time.sleep(0.1)
 
+# s7.write("8."*TOTAL_CHARS)
+# s7.set_brightness(10)
+
 for i in range(1, 11):
     s7.set_brightness(i)
     time.sleep(1)
+
+s7.clear()
